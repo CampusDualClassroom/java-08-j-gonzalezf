bb55fd43c687c2a2622435dcb80c5ed1fa6b3085
Jorge González Fernández <j.gonzalezf@udc.es>
2024-12-01 19:07:21 +0100

    Uso de while

--- src/main/java/com/campusdual/classroom/Exercise08.java
+++ src/main/java/com/campusdual/classroom/Exercise08.java
@@ -3,15 +3,14 @@ package com.campusdual.classroom;
 public class Exercise08 {
 
     public static void main(String[] args) {
-
+        whileLoop(15);
     }
 
-    //TODO ↓
-    // Comprueba el funcionamiento del bucle while, que para en cada iteración muestre el siguiente mensaje:
-    // X < Y. El próximo ciclo valdrá: X+1
-    // X+1 < Y. El próximo ciclo valdrá: X+2
-    // etc.
+    // FIXME Añadir tildes en el test para "próximo" y "valdrá" :)
     public static void whileLoop(int num) {
-
+        int x = 0;
+        while (x < num) {
+            System.out.println(x + " < " + num + ". El proximo ciclo valdra: " + ++x);
+        }
     }
 }
